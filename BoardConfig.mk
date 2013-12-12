1c724558ee24c23438835c9fd1ce749e3e139b18
AndroidMeda <hephappy@gmail.com>
2013-12-12 10:39:50 +0100

    cooper: 5MP Camera
    Change-Id: I58609f3c6ef453d88d80ed7d0143522ea3497333

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,3 +31,6 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
 ## Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/cooper/bluetooth
 
+## 5MP Camera
+BOARD_CAMERA_5MP := true
+
